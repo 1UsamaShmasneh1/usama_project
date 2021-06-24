6402d42208e1e4ab4a6d0b39f76a601ca6c29f7f
usama shmasneh <us.05.07.91@gmail.com>
2021-06-24 21:11:42 +0300

    array[,] and array[][]

---
+++ 4_array[ , ]/4_array[,].cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_array_____
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region array[,] build
+            int[,] myarr = new int[4,8];
+            myarr[2, 3] = 5;
+            for(int i = 0; i < myarr.GetLength(0); i++)
+            {
+                for(int j = 0; j < myarr.GetLength(1); j++)
+                {
+                    myarr[i, j] = 1;
+                }
+            }
+            #endregion
+
+            #region 1
+            int[,] culc = new int[10, 10] 
+            {
+                {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+                {2, 4, 6, 8, 10, 12, 14, 16, 18, 20},
+                {3, 6, 9, 12, 5, 6, 7, 8, 9, 10},
+                {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+                {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+                {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+                {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+                {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+                {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+                {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+            };
+            for (int i = 0; i <= culc.GetLength(0); i++)
+            {
+                for (int j = 0; j <= culc.GetLength(1); j++)
+                {
+                    Console.Write($"{culc[i,j]}\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+            #endregion
+        }
+    }
+}
---
+++ 5_arr[][]/5_array[][].cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_arr____
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region arr[][] build
+            string[][] myarr = new string[2][]; //   string[][,] myarr = new string[2][,];
+            myarr[0] = new string[3] {"f", "g", "d"};
+            #endregion
+        }
+    }
+}
