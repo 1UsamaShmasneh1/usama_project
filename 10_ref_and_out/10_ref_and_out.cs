27a732287d814bc30deb72291b13d87243105ffe
usama shmasneh <us.05.07.91@gmail.com>
2021-07-21 16:21:14 +0300

    ref, out HomeWork

---
+++ 10_ref_and_out/10_ref_and_out.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_ref_and_out
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int y = 5;
+            Pluse(ref y);
+
+            int z;
+            Pluse2(out z);
+
+
+            string s;
+            int res;
+            do
+            {
+                Console.WriteLine("enter number");
+                s = Console.ReadLine();
+            }
+            while (int.TryParse(s, out res));
+
+
+            PrintNumbers(3, 5, 8);
+        }
+        static void Pluse(ref int x)
+        {
+            x++;
+        }
+
+
+        static void Pluse2(out int b)
+        {
+            b = 7;
+            b++;
+        }
+
+        static bool InsertToDB(string name, int age, out string errorMessage)
+        {
+            errorMessage = "";
+            if (string.IsNullOrEmpty(name))
+            {
+                errorMessage += "name is null or empty";
+            }
+            if(age == 0)
+            {
+                errorMessage += "\nage is 0";
+            }
+            return string.IsNullOrEmpty(name);
+        }
+
+
+        static void PrintNumbers(params int[] numbers)
+        {
+
+        }
+
+        static void PrintNumber(in int numbers)
+        {
+
+        }
+
+
+        static void PrintNumber2(string error, bool printMessage = true)
+        {
+            if(printMessage)
+                Console.WriteLine("");
+        }
+
+
+        static string BuildSentence(params string[] str)
+        {
+            string str2 = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                str2 += str[i] + " ";
+            }
+            return str2;
+        }
+
+
+        static void PrintNameAndIncreaseAge(params Person[] persons)
+        {
+            Person person = new Person();
+            string str2 = "";
+            for (int i = 0; i < persons.Length; i++)
+            {
+                Console.WriteLine(persons[i].name);
+                person.IncreasAge(persons[i]);
+            }
+        }
+    }
+    class Person
+    {
+        public int age;
+        public string name;
+        public void IncreasAge(in Person person)
+        {
+            person.age += 1;
+        }
+    }
+}
---
+++ ref_out_HomeWork/ref_out_HomeWork.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ref_out_HomeWork
+{
+    class ref_out_HomeWork
+    {
+        static void Main(string[] args)
+        {
+            #region 2
+            int a1 = 4;
+            int b1 = 3;
+            PlusMulti(ref a1, ref b1);
+            Console.WriteLine($"{a1}, {b1}");
+            #endregion
+
+            #region 3
+            int a2;
+            int b2;
+            GetValue(out a2, out b2);
+            Console.WriteLine($"{a2}, {b2}");
+            #endregion
+
+            #region 4
+            string errorMessage;
+            bool isTrue = ValidateValues("usama", "shmasneh", "0525455876", "301548756", 30, out errorMessage);
+            Console.WriteLine(isTrue);
+            #endregion
+        }
+        #region 2
+        public static void PlusMulti(ref int a, ref int b)
+        {
+            a++;
+            b *= 2;
+        }
+        #endregion
+
+        #region 3
+        public static void GetValue(out int a, out int b)
+        {
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+        }
+        #endregion
+
+        #region 4
+        public static bool ValidateValues(string firstName, string lastName, string teleNum, string id, int age, out string errorMessage)
+        {
+            if (string.IsNullOrEmpty(firstName))
+            {
+                errorMessage = "enter valid firstName";
+                return false;
+            }
+            if (string.IsNullOrEmpty(lastName))
+            {
+                errorMessage = "enter valid lastName";
+                return false;
+            }
+            if (Convert.ToInt32(teleNum[0]) == 0 && teleNum.Length >= 9)
+            {
+                errorMessage = "enter valid teleNum";
+                return false;
+            }
+            if (id.Length == 9)
+            {
+                errorMessage = "enter valid id";
+                return false;
+            }
+            if (age >= 1 && age <= 100)
+            {
+                errorMessage = "enter valid age";
+                return false;
+            }
+            errorMessage = "pass";
+            return true;
+        }
+        #endregion
+    }
+}
